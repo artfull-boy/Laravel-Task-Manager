e933d496e45579807b5556e959e58cb1d97a6dc9
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-10-29 16:14:08 +0000

    Update UpdateProjectRequest.php

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProjectRequest extends FormRequest
@@ -11,7 +12,7 @@ class UpdateProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
@@ -22,7 +23,10 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name"=> "required",
+            "status"=> ["required",Rule::in(["completed","pending","in_progress"])],
+            "description"=>"required",
+            "due_date"=>"required",
         ];
     }
 }
