59e420278d446012df666a9339ff63bbb461a1bb
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-10-30 10:36:31 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Ilias',
-            'email' => 'ilias@example.com',
-            'password' => '123456789',
-            'email_verified_at' => time(),
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Meriem',
+        //     'email' => 'meriem@example.com',
+        //     'password' => '123456789',
+        //     'email_verified_at' => time(),
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'Mohammed',
+        //     'email' => 'Mohammed@example.com',
+        //     'password' => '123456789',
+        //     'email_verified_at' => time(),
+        // ]);
 
         Project::factory()->count(30)->hasTasks(30)->create();
+
     }
 }
