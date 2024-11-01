af0902f3488ea258d71b5c8770b10f2d88e61f50
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-11-01 16:40:17 +0000

    Update TaskResource.php

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -23,6 +23,7 @@ class TaskResource extends JsonResource
             'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
             'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
             'status' => $this->status,
+            'priority' => $this->priority,
             'image_path' => $this->image_path,
             'project' => new ProjectResource($this->project),
             'createdBy' => new UserResource($this->createdBy),
