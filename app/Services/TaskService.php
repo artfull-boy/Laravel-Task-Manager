a8399dd522c1d6974cb700466ce46325f5dd2d1a
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-11-01 16:40:22 +0000

    Update TaskService.php

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -4,13 +4,16 @@ namespace App\Services;
 use App\Models\Task;
 
 class TaskService {
-    public function transform($name,$status,$sortField,$direction) {
+    public function transform($name,$status,$sortField,$direction,$priority) {
         $query = Task::query();
         if ($name) {
             $query->where("name","like","%". $name ."%");
         }
         if ($status) {
             $query->where("status", $status);
+        }
+        if ($priority) {
+            $query->where("priority", $priority);
         }
         $tasks = $query->orderBy($sortField,$direction)->paginate(10);
         return $tasks;
