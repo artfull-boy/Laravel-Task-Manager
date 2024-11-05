60068ceb5549719f260d19f82c5fd2e960591f11
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-11-05 11:54:03 +0000

    Update TaskService.php

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -18,4 +18,19 @@ class TaskService {
         $tasks = $query->orderBy($sortField,$direction)->paginate(10);
         return $tasks;
     }
+    public function transformMyTasks($user,$name,$status,$sortField,$direction,$priority) {
+        $query = Task::query();
+        $query->where("assigned_user_id",$user);
+        if ($name) {
+            $query->where("name","like","%". $name ."%");
+        }
+        if ($status) {
+            $query->where("status", $status);
+        }
+        if ($priority) {
+            $query->where("priority", $priority);
+        }
+        $tasks = $query->orderBy($sortField,$direction)->paginate(10);
+        return $tasks;
+    }
 }
